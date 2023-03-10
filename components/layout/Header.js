a03bb89da7b25ad1e87488bb3aa0582b104a87e8
Jorge Osvaldo Redonda Rodríguez <jorgeosvaldorr94@gmail.com>
2023-03-10 19:02:41 -0300

    Configuring Header

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -2,46 +2,100 @@ import React from 'react';
 import Buscar from '../ui/Buscar';
 import Navegacion from './Navegacion';
 import Link from 'next/link';
-import styled from 'styled-components';
-import { jsx, css } from '@emotion/react';
+import styled from '@emotion/styled';
+import Boton from '../ui/Boton';
 
 const StyledHeader = styled.header`
-  border-bottom: 2px solid var(--gris3);
+  border-bottom: 4px solid var(--gris3);
   padding: 1rem 0;
-  width: 100%;
+`;
+
+const ContenedorHeader = styled.div`
+    max-width: 1200px;
+    width: 95%;
+    margin: 0 auto;
+    @media (min-width: 768px) {
+        display: flex;
+        justify-content: space-between;
+    }
+`;
+
+const Logo = styled.p`
+    color: var(--naranja);
+    font-size: 4rem;
+    line-height: 0;
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
+const Parrafo = styled.p`
+    color: var(--gris1);
+    font-weight: 700;
+    font-family: 'Roboto Slab', serif;
+    margin-right: 2rem;
+`;
+
+const Great = styled.div`
+    display: flex;
+    align-items: center;
+`;
+
+const LeftPanel = styled.div`
+    display: flex;
+    align-items: center;
 `;
 
 const Header = () => {
+
+    const usuario = true;
+
     return ( 
-        <header
-            css={css`
-                border-bottom: 2px solid var(--gris3);
-                padding: 1rem 0;
-            `}
-        >
-            <div>
-                <div>
-                    <p>P</p>
+        <StyledHeader>
+            <ContenedorHeader>
+                <LeftPanel>
+                    <Link href='/'>
+                        <Logo>P</Logo>
+                    </Link>
 
                     {/* Buscador aquí */}
                     <Buscar/>
 
                     {/* Navegacion Aqui */}
                     <Navegacion/>
-                </div>
+                </LeftPanel>
 
-                <div>
-                    {/* Menu de administration */}
-                    <p>Hola: Cliente</p>
+                <Great>
+                    {usuario
+                        ?
+                            (
+                                <>
+                                    {/* Menu de administration */ }
+                                    <Parrafo>Hello: Cliente</Parrafo>
+                                    <Boton bgColor='true'>
+                                        <Link href="/">Logout</Link>
+                                    </Boton>
+                                </>
+                            )
+                        : 
+                            (
+                                <>
+                                    {/* Menu de administration */ }
+                                    <Boton bgColor='true'>
+                                        <Link href="/">Login</Link>
+                                    </Boton>
 
-                    <button type='button'>Cerrar Sesión</button>
+                                    <Boton>
+                                        <Link href="/">Create Account</Link>
+                                    </Boton>
+                                </>
+                            )
+                    }
 
-                    <Link href="/">Login</Link>
-                    <Link href="/">Crear Cuenta</Link>
-                </div>
-            </div>
-        </header>
+                </Great>
+            </ContenedorHeader>
+        </StyledHeader>
      );
 }
  
-export default Header;
+export default Header; 
+
